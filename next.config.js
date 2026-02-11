a6808e81d3aafb68a438bb3dfbdd2ac703a8c6a5
Pavan Kumar Etta <pavankumaretta@Pavans-MacBook-Air-64.local>
2026-02-11 00:15:13 -0500

    Fix Tailwind production build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/crs-frontend',
 };
 
 module.exports = nextConfig;
+
+
+//past code-02
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: '/crs-frontend',
+// };
+
+// module.exports = nextConfig;
 
 /**Past code */
 // /** @type {import('next').NextConfig} */
